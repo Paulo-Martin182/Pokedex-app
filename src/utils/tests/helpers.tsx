af92efe41b82d83111ad7b8fd0dcb3858d021a0b
Paulo Martin <paulomn2013@gmail.com>
2021-02-06 21:40:04 -0400

    Criando apollo client e helper de teste

---
+++ src/utils/tests/helpers.tsx
@@ -0,0 +1,6 @@
+import { ThemeProvider } from 'styled-components'
+import { render, RenderResult } from '@testing-library/react'
+import { combineTheme } from 'styles/themes'
+
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
+  render(<ThemeProvider theme={combineTheme}>{children}</ThemeProvider>)
